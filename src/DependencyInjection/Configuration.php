b885b087cddc52ea345636ab62f2ed63dea11551
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2021-07-07 18:57:31 +0200

    set default ldap group name

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,9 +20,9 @@ class Configuration implements ConfigurationInterface
 //                ->scalarNode('LDAP_BASE_DN')->defaultValue('DC=DOMAIN,DC=net')->end()
 //                ->scalarNode('LDAP_SEARCH_DN')->defaultValue('CN=LDAPUSER,CN=Users,DC=pasaia,DC=net')->end()
 //                ->scalarNode('LDAP_PASSWD')->defaultValue('passwd')->end()
-                ->scalarNode('LDAP_ADMIN_TALDEAK')->defaultValue('ROL-Antolakuntza_Informatika, Domain Users')->end()
-                ->scalarNode('LDAP_KUDEATU_TALDEAK')->defaultValue('ROL-Antolakuntza_Informatika')->end()
-                ->scalarNode('LDAP_USER_TALDEA')->defaultValue('Domain Users')->end()
+                ->scalarNode('LDAP_ADMIN_TALDEAK')->defaultValue('LDAP_TALDEA')->end()
+                ->scalarNode('LDAP_KUDEATU_TALDEAK')->defaultValue('LDAP_TALDEA')->end()
+                ->scalarNode('LDAP_USER_TALDEA')->defaultValue('LDAP_TALDEA')->end()
                 ->scalarNode('route_after_successfull_login')->defaultValue('default')->end()
             ->end();
 
